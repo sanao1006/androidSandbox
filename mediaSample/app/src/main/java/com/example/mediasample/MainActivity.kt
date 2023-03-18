7ba419afc7142d61ee354de1b6a87f3003f3f3cb
sanao1006 <naosak1006@gmail.com>
2023-03-18 22:16:33 +0900

    implementation of onCreate

--- mediaSample/app/src/main/java/com/example/mediasample/MainActivity.kt
+++ mediaSample/app/src/main/java/com/example/mediasample/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mediasample
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var _player: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _player = MediaPlayer()
+        val mediaFileUriStr = "android.resource://${packageName}/${R.raw.sound_environment}"
+        val mediaFileUri = Uri.parse(mediaFileUriStr)
+
+        _player?.let{
+            it.setDataSource(this@MainActivity,mediaFileUri)
+            it.setOnPreparedListener(PlayerPreparedListener())
+            it.prepareAsync()
+        }
+    }
+
+    private inner class PlayerPreparedListener : MediaPlayer.OnPreparedListener {
+        override fun onPrepared(mp: MediaPlayer?) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
