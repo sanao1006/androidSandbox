c44fdfbb97fdb759403b308086cdb83be515ffd3
sanao1006 <naosak1006@gmail.com>
2023-03-19 22:17:44 +0900

    Creating a Notification Channel

--- ServiceSample/app/src/main/java/com/example/servicesample/SoundManageService.kt
+++ ServiceSample/app/src/main/java/com/example/servicesample/SoundManageService.kt
@@ -1,5 +1,7 @@
 package com.example.servicesample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
@@ -7,6 +9,9 @@ import android.net.Uri
 import android.os.IBinder
 
 class SoundManageService : Service() {
+    companion object {
+        private const val CHANNEL_ID = "soundmanagerservice_notification_channel"
+    }
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -15,6 +20,12 @@ class SoundManageService : Service() {
     private var _player: MediaPlayer? = null
     override fun onCreate() {
         _player = MediaPlayer()
+        val name = getString(R.string.notification_channel_name)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel =NotificationChannel(CHANNEL_ID, name, importance)
+//      NotificationManagerオブジェクトを取得
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
