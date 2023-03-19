4a92e7efaa1dc06b9e1abdb05a4251dc51ae5c4a
sanao1006 <naosak1006@gmail.com>
2023-03-19 17:39:11 +0900

    put Service  - SoundManageService  - implementation onStartCommand function

---
+++ ServiceSample/app/src/main/java/com/example/servicesample/SoundManageService.kt
@@ -0,0 +1,46 @@
+package com.example.servicesample
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.IBinder
+
+class SoundManageService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    private var _player: MediaPlayer? = null
+    override fun onCreate() {
+        _player = MediaPlayer()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val mediaFileUrlStr = "android.resource://${packageName}/${R.raw.sound_environment}"
+        val mediaFileUrl = Uri.parse(mediaFileUrlStr)
+        _player?.let {
+//          メディアプレーヤーに音声ファイルを指定
+            it.setDataSource(this@SoundManageService,mediaFileUrl)
+            it.setOnPreparedListener(PlayerPreparedListener())
+            it.setOnCompletionListener(PlayerCompletionListener())
+            it.prepareAsync()
+        }
+        return START_NOT_STICKY
+    }
+
+    class PlayerCompletionListener : MediaPlayer.OnCompletionListener {
+        override fun onCompletion(mp: MediaPlayer?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    private inner class PlayerPreparedListener : MediaPlayer.OnPreparedListener {
+        override fun onPrepared(mp: MediaPlayer?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+}
