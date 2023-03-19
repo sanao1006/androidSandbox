d2419293fbabe6705622b97e0537290f5729bc3a
sanao1006 <naosak1006@gmail.com>
2023-03-19 18:03:54 +0900

    Implementation of behavior when tapping the playback button  - onPlayButtonClick function in MainActivity.kt

--- ServiceSample/app/src/main/java/com/example/servicesample/MainActivity.kt
+++ ServiceSample/app/src/main/java/com/example/servicesample/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.servicesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onPlayButtonClick(view: View){
+        val intent = Intent(this@MainActivity,SoundManageService::class.java)
+        startService(intent)
+
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = true
+        btStop.isEnabled = true
     }
 }
