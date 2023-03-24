13c7e03cbea9c1d8fbc52fe4c888cec231589bad
sanao1006 <naosak1006@gmail.com>
2023-03-24 13:19:11 +0900

    section4

--- BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiclayoutscodelab.ui.theme.BasicLayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +26,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp),
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null)},
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(id = R.string.placeholder_search))
+        }
+    )
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicLayoutsCodelabTheme {
-        Greeting("Android")
-    }
 }
