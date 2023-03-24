9f75f24a56a075a93e071991e3689995260714af
sanao1006 <naosak1006@gmail.com>
2023-03-24 11:07:46 +0900

    section10

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,13 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier){
-    StatefulCounter(modifier = modifier)
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        StatefulCounter(modifier = modifier)
+        WellnessTasksList()
+    }
 }
