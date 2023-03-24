a3909f3c9a126763af12512c07725fed41dbe498
sanao1006 <naosak1006@gmail.com>
2023-03-24 10:17:49 +0900

    section6

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCount.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCount.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -14,11 +15,23 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 //        val count: MutableState<Int> = remember { mutableStateOf(0) }
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
             Text(text = "You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(16.dp), enabled = count < 10) {
-            Text(text = "Add one")
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text(text = "Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text(text = "Clear count")
 
+            }
         }
 
     }
