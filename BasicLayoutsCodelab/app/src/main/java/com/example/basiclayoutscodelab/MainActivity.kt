6b3a9b6e65d76577993de831430e36a249e2ea03
sanao1006 <naosak1006@gmail.com>
2023-03-24 15:35:42 +0900

    7

--- BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -38,6 +40,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
 
 @Preview
 @Composable
@@ -131,4 +156,20 @@ fun FavoriteCollectionCard(
 
     }
 
+}
+
+@Preview
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        items(alignYourBodyData){ item ->
+            AlignYourBodyElement(drawable = item.drawable, text = item.text)
+        }
+    }
+    
 }
