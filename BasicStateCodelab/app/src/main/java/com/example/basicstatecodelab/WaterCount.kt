73a6faa7b854705a912321fbb02bf76a5f26fa29
sanao1006 <naosak1006@gmail.com>
2023-03-24 09:29:27 +0900

    section4

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCount.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCount.kt
@@ -1,17 +1,23 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = Modifier.padding(16.dp)
-    )
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text(text = "You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(16.dp)) {
+            Text(text = "Add one")
+            
+        }
+
+    }
 
 }
